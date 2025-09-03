1b760c5b1ee80e6ab9fa6061832df04d342a12de
marcel <lis.marcys@gmail.com>
2025-09-03 09:38:11 +0200

    [feat] added operation result class

---
+++ src/Shared/FoxSky.StocksSystem.OperationResult/OperationResult.cs
@@ -0,0 +1,20 @@
+﻿namespace FoxSky.StocksSystem.OperationResult;
+
+public class OperationResult
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public object? Data { get; set; } = null;
+
+    public OperationResult() { }
+
+    public static OperationResult Succeeded(string message = "Operation succeeded.", object? data = null)
+    {
+        return new OperationResult { Success = true, Message = message, Data = data };
+    }
+
+    public static OperationResult Failed(string message = "Operation failed.", object? data = null)
+    {
+        return new OperationResult { Success = false, Message = message, Data = data };
+    }
+}
