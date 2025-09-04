e8acd1dc21e2aa1f79cd06a72be1414fbbdaa360
marcel <lis.marcys@gmail.com>
2025-09-04 12:38:43 +0200

    [feat] implemented service for using .env files

---
+++ src/Shared/FoxSky.StocksSystem.EnvReader/EnvReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace FoxSky.StocksSystem.EnvReader;
+
+public class EnvReader
+{
+    public static void Load()
+    {
+        var filePath = FindEnvFile();
+
+        if (!File.Exists(filePath) || filePath == null)
+            throw new FileNotFoundException($"The file '{filePath}' does not exist.");
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                continue; // Skip empty lines and comments
+
+            var parts = line.Split('=', 2);
+            if (parts.Length != 2)
+                continue; // Skip lines that are not key-value pairs
+
+            var key = parts[0].Trim();
+            var value = parts[1].Trim();
+            Environment.SetEnvironmentVariable(key, value);
+        }
+    }
+
+    private static string? FindEnvFile()
+    {
+        DirectoryInfo currentDir = new(AppDomain.CurrentDomain.BaseDirectory);
+
+        while (currentDir != null)
+        {
+            string envPath = Path.Combine(currentDir.FullName, ".env");
+            if (File.Exists(envPath))
+            {
+                return envPath;
+            }
+
+            currentDir = currentDir.Parent!;
+        }
+
+        return null;
+    }
+}
