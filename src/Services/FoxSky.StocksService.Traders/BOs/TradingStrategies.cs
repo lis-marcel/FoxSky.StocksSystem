c28ee1796d4311c93f164f3f19ac0e9c4048c7c0
marcel <lis.marcys@gmail.com>
2025-09-12 09:54:10 +0200

    [feat] introduces trading strategies class

---
+++ src/Services/FoxSky.StocksService.Traders/BOs/TradingStrategies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxSky.StocksService.Traders.BOs
+{
+    public class TradingStrategies
+    {
+        public string Ticker { get; set; } = string.Empty;
+        public TradingAction Action { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public decimal Confidence { get; set; }
+        public DateTime DecisionTime { get; set; } = DateTime.UtcNow;
+    }
+
+    public enum TradingAction
+    {
+        Buy,
+        Sell,
+        Hold
+    }
+}
