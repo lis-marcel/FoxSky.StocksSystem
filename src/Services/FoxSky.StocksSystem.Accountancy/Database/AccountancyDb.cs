bfdad47a10570eecfb85cd4bf74b25e46dd89f37
marcel <lis.marcys@gmail.com>
2025-09-23 14:05:10 +0200

    [feat] added inital file for db context

---
+++ src/Services/FoxSky.StocksSystem.Accountancy/Database/AccountancyDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxSky.StocksSystem.Accountancy.Database
+{
+    public class AccountancyDb : DbContext
+    {
+        DbSet<Models.AccountancyRequest> AccountancyRequests { get; set; }
+    }
+}
