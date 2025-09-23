481c5816aa02fdf71b5f1f82c83431f429aadfce
marcel <lis.marcys@gmail.com>
2025-09-23 14:48:04 +0200

    [chore] divided files logic into adequate folder

---
+++ src/Services/FoxSky.StocksSystem.Accountancy/Database/Context/AccountancyDb.cs
@@ -0,0 +1,10 @@
+﻿using FoxSky.StocksSystem.Accountancy.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoxSky.StocksSystem.Accountancy.Database.Context
+{
+    public class AccountancyDb : DbContext
+    {
+        DbSet<Trade> Trades { get; set; }
+    }
+}
---
+++ src/Services/FoxSky.StocksSystem.Accountancy/Database/Entities/Trade.cs
@@ -0,0 +1,13 @@
+﻿using FoxSky.StocksSystem.Accountancy.Models;
+
+namespace FoxSky.StocksSystem.Accountancy.Database.Entities
+{
+    public class Trade
+    {
+        public string Ticker { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public TradingAction TradingAction { get; set; }
+        public DateTime DecisionTime { get; set; }
+    }
+}
