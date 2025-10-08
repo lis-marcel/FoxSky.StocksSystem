b8c12b3ff88cb0fe7e016bdcbe66d2f0f561e597
marcel <lis.marcys@gmail.com>
2025-10-08 23:00:07 +0200

    [feat] added pdf generator lib

---
+++ src/Services/FoxSky.StocksSystem.Accountancy/Services/ReportGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxSky.StocksSystem.Accountancy.Services
+{
+    internal class ReportGenerator
+    {
+    }
+}
