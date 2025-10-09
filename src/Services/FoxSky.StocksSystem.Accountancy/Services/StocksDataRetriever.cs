a89359213ab8d7e708c39f0bdd844ea7b8ea6f30
marcel <lis.marcys@gmail.com>
2025-10-09 22:26:04 +0200

    [feat] added helper method for retrieving reoprt data

---
+++ src/Services/FoxSky.StocksSystem.Accountancy/Services/StocksDataRetriever.cs
@@ -0,0 +1,24 @@
+﻿using FoxSky.StocksSystem.Accountancy.Database.Context;
+using FoxSky.StocksSystem.Accountancy.Database.Entities;
+using FoxSky.StocksSystem.Accountancy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxSky.StocksSystem.Accountancy.Services
+{
+    public class StocksDataRetriever
+    {
+        public static async Task<List<Trade>> RetreiveTradesData(AccountancyDbContext dbContext, DateTime beginningDate, DateTime endDate)
+        {
+            var trades = await dbContext.Trades
+                .Where(e => e.DecisionTime > beginningDate && e.DecisionTime < endDate)
+                .ToListAsync();
+
+            return trades;
+        }
+    }
+}
