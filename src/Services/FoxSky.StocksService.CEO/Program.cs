164cd07ecb9a251ed25eb623cda943d82aa6886e
marcel <lis.marcys@gmail.com>
2025-10-14 09:00:23 +0200

    [feat] craeted initial CEOs service

---
+++ src/Services/FoxSky.StocksService.CEO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FoxSky.StocksService.CEO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
