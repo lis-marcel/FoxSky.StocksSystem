236ad8139ba4622633b4788bda00e3d8206172a4
marcel <lis.marcys@gmail.com>
2025-10-14 08:52:43 +0200

    [feat] added initial class for storing the report gernerator methods

--- src/Services/FoxSky.StocksSystem.Accountancy/Services/ReportGenerator.cs
+++ src/Services/FoxSky.StocksSystem.Accountancy/Services/ReportGenerator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FoxSky.StocksSystem.Accountancy.Services
+﻿namespace FoxSky.StocksSystem.Accountancy.Services
 {
     internal class ReportGenerator
     {
