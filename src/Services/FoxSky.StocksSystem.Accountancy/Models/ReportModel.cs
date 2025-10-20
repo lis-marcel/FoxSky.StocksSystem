de1764d99f18d843513d53349f74a3b295019e13
Marcel Lis <lis.marcys@gmail.com>
2025-10-20 10:18:27 +0100

    [feat] added initial report data model

---
+++ src/Services/FoxSky.StocksSystem.Accountancy/Models/ReportModel.cs
@@ -0,0 +1,12 @@
+﻿using FoxSky.StocksSystem.Accountancy.Database.Entities;
+
+namespace FoxSky.StocksSystem.Accountancy.Models
+{
+    public class ReportModel
+    {
+        public int ReportId { get; set; }
+        public string IssuerName { get; set; } = "CEO";
+        public DateTime IssueDate { get; set; }
+        public List<Trade>? Trades { get; set; }
+    }
+}
