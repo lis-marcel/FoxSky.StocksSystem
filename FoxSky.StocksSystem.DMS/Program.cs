e225da63ead9c83ccab435a8bfe65f1f6f4eff4f
Marcel Lis <lis.marcys@gmail.com>
2025-11-04 13:51:09 +0100

    [feat] initialized DMS project

---
+++ FoxSky.StocksSystem.DMS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FoxSky.StocksSystem.DMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, DMS World!");
+        }
+    }
+}
