798f16bd25b34201fee41a8c41d2cfab8ff116d0
Marcel Lis <lis.marcys@gmail.com>
2025-11-18 18:21:38 +0100

    [feat] added initaial services files

---
+++ src/Services/FoxSky.StocksSystem.DMS/Services/DMSService.cs
@@ -0,0 +1,15 @@
+﻿using FoxSky.StocksSystem.SharedServices;
+using FoxSky.StocksSystem.SharedServices.Models;
+
+namespace FoxSky.StocksSystem.DMS.Services
+{
+    public class DMSService : IDMSService
+    {
+        public Task<OperationResult> ProcessSaveDocumentRequest(byte[] data)
+        {
+            //var deserializedData = Newtonsoft.Json.Converters.Convert.DeserializeObject<>(
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/FoxSky.StocksSystem.DMS/Services/IDMSService.cs
@@ -0,0 +1,9 @@
+﻿using FoxSky.StocksSystem.SharedServices;
+
+namespace FoxSky.StocksSystem.DMS.Services
+{
+    internal interface IDMSService
+    {
+        Task<OperationResult> ProcessSaveDocumentRequest(byte[] data);
+    }
+}
