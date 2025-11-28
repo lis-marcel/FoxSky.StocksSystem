93a3dd1e8111656d00c0dae2cbd711473f4beeb2
marcel <lis.marcys@gmail.com>
2025-11-28 16:31:09 +0100

    [feat] added new data model for fluent data exchange

---
+++ src/Shared/FoxSky.StocksService.SharedServices/Models/ReportRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FoxSky.StocksService.SharedServices.Models
+{
+    public class ReportRequestModel
+    {
+        public Guid ReportId { get; set; }
+        public int CommissionerId { get; set; } 
+        public DateTime BeginningDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
